8b2255879058aed46b4fa021a0eedc96ce70a6b5
Andreas <sajaki9@gmail.com>
2013-05-22 18:17:35 +0200

    removed $default_function from acp_sfnc.php

--- root/includes/acp/acp_sfnc.php
+++ root/includes/acp/acp_sfnc.php
@@ -249,7 +249,7 @@ class acp_sfnc
 						$template->assign_vars(array(
 							// main info
 							'FEED_ID' => $id,
-							'FEED_NAME' => isset($list[$id]['feed_name']) ? $list[$id]['feed_name'].$default_function : '',
+							'FEED_NAME' => isset($list[$id]['feed_name']) ? $list[$id]['feed_name'] : '',
 							'FEED_TYPE' => isset($list[$id]['feed_type']) ? $list[$id]['feed_type'] : '',
 							'URL' => isset($list[$id]['url']) ? $list[$id]['url'] : '',
 							'ENABLED_POSTING' => isset($list[$id]['enabled_posting']) ? $list[$id]['enabled_posting'] : '',
